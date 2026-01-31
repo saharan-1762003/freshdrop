ee909271e3f7b4a617208d90c38f9f5a6ad3e9e3
Ayush13999 <singhalayush13999@gmail.com>
2026-01-31 15:37:30 +0530

    first commit

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-interface IUser{
+export interface IUser{
     _id?:mongoose.Types.ObjectId
     name:string
     email:string
@@ -8,6 +8,17 @@ interface IUser{
     mobile?:string
     role:"user" | "deliveryBoy" | "admin"
     image?:string
+    location?: {
+    type: {
+      type: StringConstructor;
+      enum: string[];
+      default: string;
+    };
+    coordinates: {
+      type: NumberConstructor[];
+      default: number[];
+    };
+  };
 }
 
 const userSchema=new mongoose.Schema<IUser>({
@@ -35,9 +46,22 @@ const userSchema=new mongoose.Schema<IUser>({
     },
     image:{
         type:String
-    }
+    },
+    location: {
+      type: {
+        type: String,
+        enum: ["Point"],
+        default: "Point",
+      },
+      coordinates: {
+        type: [Number],
+        default: [0, 0],
+      },
+    },
 
 },{timestamps:true})
+
+userSchema.index({ location: "2dsphere" });
 
 const User=mongoose.models.User || mongoose.model("User",userSchema)
 export default User
